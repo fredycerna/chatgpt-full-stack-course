5ea31fdc43e8c427b1c3b6d8c07bb7539fafcce0
fredycerna <fredycerna@gmail.com>
2023-03-28 11:19:41 -0600

    initial express route

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my game app');
+});
+
 app.listen(port, ()=> console.log('Server listening on port 3000'));
-
-
